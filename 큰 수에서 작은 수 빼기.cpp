a0de211ef4d2cb78cfb2aa34ff7728e845fefa3b
chanmi0205 <chanmi1155@naver.com>
2021-07-19 00:15:07 +0900

    first commit

---
+++ 큰 수에서 작은 수 빼기.cpp
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+#include <stdio.h>
+main ( ) {
+	int a, b, k;
+	int m[8] = { 4, 12, 19, 5, 8, 6, 3, 7 };
+	a = m[0];
+	b = m[0];
+	for ( k=0; k<=7; k ++ )  {
+		if ( m[k] > a ) 
+		a = m[k];
+	else
+		if ( m[k] < b )
+		b = m[k];
+	}
+	printf ("%d", a-b);
+}
+=======
+#include <stdio.h>
+main ( ) {
+	int a, b, k;
+	int m[8] = { 4, 12, 19, 5, 8, 6, 3, 7 };
+	a = m[0];
+	b = m[0];
+	for ( k=0; k<=7; k ++ )  {
+		if ( m[k] > a ) 
+		a = m[k];
+	else
+		if ( m[k] < b )
+		b = m[k];
+	}
+	printf ("%d", a-b);
+}
+>>>>>>> 4084420bc648ae16c3f7ec07265dac2fbd8c3f5f
