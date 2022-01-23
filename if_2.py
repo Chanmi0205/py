185c1c5dc928fe686ee65cf98781b54e7b0b78fa
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2022-01-23 22:21:59 +0900

    Add files via upload

---
+++ if_2.py
@@ -0,0 +1,13 @@
+money = True
+if money:
+    print("닭강정을 사먹자!")
+else:
+    print("사먹지 말자ㅠㅠ")
+
+money_2 = 15000
+
+if (money_2 >= 15000):
+    print("닭강정을 사먹자!!")
+else:
+    print("돈도 없으니 절약하자 ㅠㅠ")
+    
